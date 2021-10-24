6f84c551bab37bb309771e6941d4aed18b284535
Dr. Chaojie Wang <wcj365@gmail.com>
2021-10-24 22:46:49 +0000

    .

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
+if [ -d "_build" ] 
+then
+    rm -r _build 
+fi
+
 jupyter-book build --path-output . docs2
+
+if [ -d "docs" ] 
+then
+    rm -r docs 
+fi
+
+git add .
+git commit -m "."
+git push
+
 mkdir docs
-mv -r _build/html/* docs/
+
+cp -r _build/html/* docs/
 cp .nojekyll docs/
 rm -r _build/
+
 git add .
 git commit -m "."
 git push
