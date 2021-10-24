a35d681ebf50836a6a0f69bf224ae4e4cc523777
Dr. Chaojie Wang <wcj365@gmail.com>
2021-10-24 22:41:16 +0000

    .

--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
 jupyter-book build --path-output . docs2
+mkdir docs
+mv -r _build/html/* docs/
+cp .nojekyll docs/
+rm -r _build/
+git add .
+git commit -m "."
+git push
--- docs/_sources/index.md
+++
@@ -1,6 +0,0 @@
-# 诗文集
-
-![](xiang.png)
-![](chou.png)
-![](yong.png)
-![](jiu.png)
