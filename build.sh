dc466cd498af03077007fcace877d2d2999385cb
Dr. Chaojie Wang <wcj365@gmail.com>
2021-11-02 19:25:17 +0000

    Built the static website of the book.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/bash
+
 if [ -d "_build" ] 
 then
     rm -r _build 
@@ -8,15 +10,28 @@ then
     rm -r docs 
 fi
 
+# Build the static website for the book
+
 jupyter-book build --path-output . src
 mkdir docs
 cp -r _build/html/* ./docs/
 cp .nojekyll docs/
 rm -r _build/
 
+# zip up website contents (not to include docs folder name)
+
+cd docs
+zip -r ../website.zip .
+cd ../
+
+
+# Build the PDF version of the book
+
 jupyter-book build --path-output . src --builder pdfhtml
 cp _build/pdf/book.pdf ./pdf/
 rm -r _build/
+
+# Push the changes to GitHub
 
 git add .
 git commit -m "Built the static website of the book."
