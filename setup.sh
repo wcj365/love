7fd30a8db8286c3b40edeab8acfa4c5713edf817
Dr. Chaojie Wang <wcj365@gmail.com>
2021-11-22 02:07:28 +0000

    .

--- setup.sh
+++ setup.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/bash
 
-# Step 1
-# Install required Python packages
-
-pip install -r requirements.txt
+# Step 1 Update apt-get
+sudo apt-get update
 
 # Step 2
-# Instsall system packages required for generating PDF version using Puppeteer
-# https://developers.google.com/web/tools/puppeteer
-
-sudo apt install -y gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
-
-# Step 3 
-# Install 楷体 font for Chinese Character
+# Install 楷体 font for Chinese Character. 
+# Font name = "AR PL UKai TW"
 # https://gist.github.com/allex/11203573
-
 sudo apt-get install fonts-arphic-ukai
+
+# Step 3
+# Install latex-cjk-all  to support Chinese/Jaopanese/Korean language for LaTex 
+sudo apt install latex-cjk-all
+
+# Step 4
+# install LaTex engine (texlive) and LaTex to PDF transformation library (XeLaTex) required for Pandoc
+# texlive is a LaTex engine and xetex is a Latex to PDF transformation enginer which supports UTF-8 unicode for Chinese characters
+sudo apt install texlive-xetex
+
+# Step 5
+# Install Pandoc for generating PDF files
+sudo apt-get install pandoc
+
+# Step 6
+# Install required Python packages for Jupyter Book for generating HTML website
+pip install -r requirements.txt
