720bd1914710540b525ed8c5ef64c6a9d03ba7a5
Dr. Chaojie Wang <wcj365@gmail.com>
2021-11-30 15:35:16 +0000

    .

--- pandoc/pandoc.sh
+++ pandoc/pandoc.sh
@@ -2,17 +2,17 @@
 
 # Transform files 
 
-python pandoc.py
+python pandoc_epub.py
 
 # Generate PDF files
 
-BOOKS="classic_poems modern_poems proses english"
-#BOOKS="classic_poems"
+#BOOKS="classic_poems modern_poems proses english"
+BOOKS="classic_poems"
 
 
 for book in $BOOKS
 do
-    pandoc --pdf-engine=xelatex  `find ../_pandoc/$book -name '*.md' | sort` -o ../pdf/wcj365_$book.pdf
+#    pandoc --pdf-engine=xelatex  `find ../_pandoc/$book -name '*.md' | sort` -o ../pdf/wcj365_$book.pdf
     pandoc --pdf-engine=xelatex  `find ../_pandoc/$book -name '*.md' | sort` -o ../pdf/wcj365_$book.epub
 #    pandoc --pdf-engine=xelatex  `find ../_pandoc/$book -name '*.md' | sort` -o ../pdf/wcj365_$book.docx
 #    pandoc --pdf-engine=xelatex  `find ../_pandoc/$book -name '*.md' | sort` -o ../pdf/wcj365_$book.html
@@ -20,8 +20,8 @@ done
 
 # Push the changes to GitHub
 
-cd ../
+#cd ../
 
-git add .
-git commit -m "Built the PDF verion of the books."
-git push
+#git add .
+#git commit -m "Built the PDF verion of the books."
+#git push
